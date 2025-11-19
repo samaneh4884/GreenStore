d539c54c03b57bf494be7b29ece3762d6b4be29f
SSN4444 <saeednoferest@gmail.com>
2025-11-19 15:41:10 +0330

    init html and css and js files

---
+++ greenstore-client/js/index.js
@@ -0,0 +1,14 @@
+function goToUserSignup() {
+    window.location.href = "signup-user.html";
+}
+
+function goToStoreSignup() {
+    window.location.href = "signup-store.html";
+}
+function goToStoreLogin(){
+    window.location.href = "login-store.html";
+}
+function goToUserLogin(){
+    window.location.href = "login-user.html";
+}
+
